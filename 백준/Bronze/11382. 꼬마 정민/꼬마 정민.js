235774cd7a58e7c9252d0e21554a19f62edf2b99
IM_SORRY <136697975+JinSol-coder@users.noreply.github.com>
2025-03-26 00:27:06 +0900

    [Bronze V] Title: 꼬마 정민, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().split(' '); 
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+const C = parseInt(input[2]);
+
+console.log(A+B+C);
