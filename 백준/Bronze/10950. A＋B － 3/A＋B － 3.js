ec82530dda8e5343014e7eaebcbea732fc4e7009
IM_SORRY <136697975+JinSol-coder@users.noreply.github.com>
2025-03-31 13:25:47 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const count = parseInt(input[0]);
 
-for(let i = 1; i<=count; i++){
-	const [a, b] = input[i].split(' ').map(Number);
-	console.log(a+b);
+for(let i = 1; i <= count; i++){
+    const [a,b] = input[i].split(' ').map(Number);
+    console.log(a+b);
 }
