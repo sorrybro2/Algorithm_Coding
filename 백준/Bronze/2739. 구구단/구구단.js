986b5e1afe4fcde3757d54257a37907057d2b84f
IM_SORRY <136697975+JinSol-coder@users.noreply.github.com>
2025-03-31 11:43:01 +0900

    [Bronze V] Title: 구구단, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+const a = parseInt(input[0]);
+
+for(let i = 1; i <= 9; i++){
+    console.log(a+" * "+i+" = "+a*i);
+}
