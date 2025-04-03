ecaf4e2783b4c9f4ef72c49da519f5baf1b932b1
IM_SORRY <136697975+JinSol-coder@users.noreply.github.com>
2025-04-03 13:25:07 +0900

    [Bronze V] Title: X보다 작은 수, Time: 96 ms, Memory: 11060 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [length, target] = input[0].split(' ').map(Number);
-const nums = input[1].split(' ').map(Number);
-
+const [N, X] = input[0].split(' ').map(Number);
+const A = input[1].split(' ').map(Number);
 let answer = [];
 
-for(let i = 0; i < length; i++){
-    if(nums[i] < target){
-        answer.push(nums[i]);
+for(let i = 0; i < N; i++){
+    if(A[i] < X){
+        answer.push(A[i]);
     }
 }
 
