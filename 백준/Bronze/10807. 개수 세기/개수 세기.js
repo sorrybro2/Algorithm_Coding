6ddd6c7444b14945761da7c656ae38a33a6f5f38
IM_SORRY <136697975+JinSol-coder@users.noreply.github.com>
2025-04-03 13:18:33 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.js
+++ 백준/Bronze/10807. 개수 세기/개수 세기.js
@@ -1,16 +1,13 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
 
 const N = parseInt(input[0]);
-let nums = [];
-let cnt = 0;
-for(let i = 0; i < N; i++){
-    nums = input[1].split(' ');
-}
+const num = input[1].split(' ').map(Number);
 const v = parseInt(input[2]);
+let cnt = 0;
 
 for(let i = 0; i < N; i++){
-    if(nums[i]==v){
+    if(num[i] == input[2]){
         cnt++;
     }
 }
