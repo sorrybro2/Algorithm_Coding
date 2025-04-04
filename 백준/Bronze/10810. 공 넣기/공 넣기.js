50cf78a71c3b2ea797eeb6ba63d6dd28b5618075
IM_SORRY <136697975+JinSol-coder@users.noreply.github.com>
2025-04-04 11:27:37 +0900

    [Bronze III] Title: 공 넣기, Time: 104 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.js
+++ 백준/Bronze/10810. 공 넣기/공 넣기.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [N, M] = input[0].split(' ').map(Number);
-let baskets = Array(N).fill(0);
+let basket = Array(N).fill(0);
 
-for(let a=1; a<=M; a++){
-    const [i,j,k] = input[a].split(' ').map(Number);
-    
-    for(let a=i-1; a<j; a++){
-        baskets[a] = k;
+for(let i=0; i<M; i++){
+    const [a,b,c] = input[i+1].split(' ').map(Number);
+    for(let j=a-1; j<b; j++){
+        basket[j] = c;
     }
 }
-console.log(baskets.join(' '));
+
+console.log(basket.join(' '));
