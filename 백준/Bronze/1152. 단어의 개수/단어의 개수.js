5376f7402b403f2c79b3c8e625a54ab4a6fe10e2
IM_SORRY <136697975+JinSol-coder@users.noreply.github.com>
2025-04-17 08:59:59 +0900

    [Bronze II] Title: 단어의 개수, Time: 140 ms, Memory: 28336 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+if (input === '') {
+    console.log(0);
+} else {
+    const words = input.split(/\s+/); 
+    console.log(words.length);
+}
