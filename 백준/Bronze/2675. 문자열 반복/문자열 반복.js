4bc9631a0e7278300645f2a23d05f2d0c5987f80
IM_SORRY <136697975+JinSol-coder@users.noreply.github.com>
2025-04-16 14:17:19 +0900

    [Bronze II] Title: 문자열 반복, Time: 84 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -4,12 +4,14 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const count = parseInt(input[0]);
 
 for(let i = 1; i <= count; i++){
-    const [numStr, word] = input[i].split(' ');
-    const num = parseInt(numStr);
+    const [charNum, word] = input[i].split(' ');
+    const num = parseInt(charNum);
     
     let answer = '';
+    
     for(let char of word){
         answer += char.repeat(num);
     }
+    
     console.log(answer);
 }
