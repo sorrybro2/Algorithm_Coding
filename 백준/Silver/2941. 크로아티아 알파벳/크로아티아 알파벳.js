a73f83dac4d028cbdbfc0b0173e955222fc339d5
IM_SORRY <136697975+JinSol-coder@users.noreply.github.com>
2025-04-26 16:18:17 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim();
 
-const croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
+const crotia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
 
-for(const letter of croatia){
-   input = input.split(letter).join('a');
+for(const letter of crotia){
+    input = input.split(letter).join('A');
 }
 
 console.log(input.length);
