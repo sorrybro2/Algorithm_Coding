9cf519202ddf7cdc1fcf4e3fc3163468e58938af
IM_SORRY <136697975+JinSol-coder@users.noreply.github.com>
2025-05-02 15:33:12 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 96 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [N, M] = input[0].split(' ').map(Number);
+const [n, m] = input[0].split(' ').map(Number);
 
 const A = [];
 const B = [];
 
-for(let i = 1; i <= N; i++){
+for(let i = 1; i <= n; i++){
     A.push(input[i].split(' ').map(Number));
 }
 
-for(let i = N+1; i<= N*2; i++){
+for(let i = n + 1; i <= n*2; i++){
     B.push(input[i].split(' ').map(Number));
 }
 
 const answer = [];
 
-for(let i = 0; i < N; i++){
+for(let i = 0; i < n; i++){
     const row = [];
-    for(let j = 0; j < M; j++){
-        row.push(A[i][j] + B[i][j]);
+    for(let j = 0; j < m; j++){
+        const num = A[i][j] + B[i][j];
+        row.push(num);
     }
     answer.push(row.join(' '));
 }
 
 console.log(answer.join('\n'));
-
