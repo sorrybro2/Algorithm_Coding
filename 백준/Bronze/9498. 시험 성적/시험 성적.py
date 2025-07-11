d8f536f3b155e20d488021b2b5dd8cd74cd20b2f
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-07-11 10:25:19 +0900

    [Bronze V] Title: 시험 성적, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -2,11 +2,11 @@ exam = int(input())
 
 if exam>=90 :
     print("A")
-elif exam>=80 and exam<90 :
+elif exam>=80 :
     print("B")
-elif exam>=70 and exam<80 :
+elif exam>=70 :
     print("C")
-elif exam>=60 and exam<70 :
+elif exam>=60 :
     print("D")
 else :
     print("F")
