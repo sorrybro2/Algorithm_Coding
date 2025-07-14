47dca24ad5558c666c9a579bcefccf7fc73eb14e
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-07-14 21:25:42 +0900

    [Gold V] Title: 하노이 탑, Time: 300 ms, Memory: 121544 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,12 +1,15 @@
-def ha_noi(one, three, n):
-    if n==1:
-        print(one,three)
-        return
-    ha_noi(one, 6-three-one, n-1)
-    print(one,three)
-    ha_noi(6-three-one, three, n-1)
-
 n = int(input())
 print(2**n-1)
+
+def hanoi(start, end, n):
+    if n == 1:
+        print(f"{start} {end}")
+        return
+    else :
+        hanoi(start, 6-start-end, n-1)
+        print(f"{start} {end}")
+        hanoi(6-start-end, end, n-1)
+    
 if n <= 20:
-    ha_noi(1,3,n)
+    hanoi(1, 3, n)
+         
