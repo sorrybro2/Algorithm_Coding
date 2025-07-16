4993b7af19367ebc57e7dcb50a55c2349c9158c0
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-07-16 16:42:01 +0900

    [Gold V] Title: Z, Time: 116 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,24 +1,22 @@
-N, r, c = map(int,input().split())
-
+n, r, c = map(int,input().split())
 ans = 0
 
-while N > 0:
-    N -= 1
-    #2사분면
-    if r<2**N and c<2**N :
-        ans += (2**N)*(2**N)*0
-    #1사분면
-    elif r<2**N and c>=2**N :
-        ans += (2**N)*(2**N)*1
-        c -= (2**N)
-    #3사분면
-    elif r>=2**N and c<2**N :
-        ans += (2**N)*(2**N)*2
-        r -= (2**N)
-    #4사분면
+def z(num, x, y):
+    global ans
+    if num==0:
+        return
+    half = 2 ** (num-1)
+    if x < half and y < half:
+        z(num-1,x,y)
+    elif x < half and y >= half:
+        ans += half*half
+        z(num-1,x,y-half)
+    elif x >= half and y < half:
+        ans += half*half*2
+        z(num-1, x-half, y)
     else:
-        ans += (2**N)*(2**N)*3
-        c -= (2**N)
-        r -= (2**N)
+        ans += half*half*3
+        z(num-1, x-half, y-half)
 
+z(n,r,c)
 print(ans)
