5cfffe448a01f30c616e593e321943b0c47d37b6
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-07-18 22:37:27 +0900

    [Silver I] Title: 곱셈, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,11 +1,13 @@
 a,b,c = map(int,input().split())
-def power(x, n):
-    if n == 0:
-        return 1
-    elif n % 2 == 0:
-        half = power(x, n//2)
+
+def divide(x,y):
+    if y == 1:
+        return x % c
+    half = divide(x, y//2)
+    if y%2==0:
         return half*half%c
     else:
-        return x*power(x, n-1)%c
-
-print(power(a,b))
+        return half*half*x%c
+    
+ans = divide(a,b)
+print(ans)
