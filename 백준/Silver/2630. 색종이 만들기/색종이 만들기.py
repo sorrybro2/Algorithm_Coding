8bb2acc4fbe9ed2cedaab3cf6e59192881d3aa48
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-07-18 22:02:21 +0900

    [Silver II] Title: 색종이 만들기, Time: 116 ms, Memory: 112088 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -2,13 +2,14 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-paper = [list(map(int, input().split()))for _ in range(N)]
+paper = [list(map(int,input().split()))for _ in range(N)]
+
 white = 0
 blue = 0
 
-def divide(x, y, n):
-    global white, blue
+def divide(x,y,n):
     color = paper[x][y]
+    global white, blue
     for i in range(x, x+n):
         for j in range(y, y+n):
             if paper[i][j] != color:
@@ -18,10 +19,10 @@ def divide(x, y, n):
                 divide(x+half,y,half)
                 divide(x+half,y+half,half)
                 return
-            
+    
     if color == 0:
         white += 1
-    else:
+    else :
         blue += 1
 
 divide(0,0,N)
