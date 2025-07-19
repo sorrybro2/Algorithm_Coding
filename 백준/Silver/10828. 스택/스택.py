0315c97776b9905fd4f4774c04a69d0999c2cd99
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-07-19 21:32:58 +0900

    [Silver IV] Title: 스택, Time: 128 ms, Memory: 111444 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -6,18 +6,16 @@ stack = []
 
 for _ in range(n):
     command = input().strip()
-    if command.startswith("push"):
-        _, num = command.split()
-        stack.append(num)
-
-    elif command == "top":
+    if command.startswith('push'):
+        _, a = command.split()
+        stack.append(a)
+    elif command == 'pop':
+        print(stack.pop() if stack else -1)
+    elif command == 'size':
+        print(len(stack))
+    elif command == 'empty':
+        print(0 if stack else 1)
+    elif command == 'top':
         print(stack[-1] if stack else -1)
 
-    elif command == "empty":
-        print(1 if len(stack)==0 else 0)
 
-    elif command == "pop":
-        print(stack.pop() if len(stack) > 0 else -1)
-
-    elif command == "size":
-        print(len(stack))
