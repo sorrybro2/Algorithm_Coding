5832d4dbce25f49a3a3448065d0938ca060af803
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-07-21 13:39:32 +0900

    [Silver IV] Title: 카드2, Time: 136 ms, Memory: 128188 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,16 @@
+
+from collections import deque
+n = int(input())
+
+q = deque()
+
+for i in range(1, n+1):
+    q.append(i)
+
+sorted(q, reverse=True)
+
+while len(q) > 1:
+    q.popleft()
+    q.append(q.popleft()) 
+
+print(q[0])
