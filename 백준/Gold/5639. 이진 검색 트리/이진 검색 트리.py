2b403ddcdf521be28c8c0f852949f52fcd527006
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-07-25 17:50:40 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2616 ms, Memory: 35864 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,20 +1,25 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-preorder = list(map(int, sys.stdin.buffer.read().split()))
+preorder = list(map(int, sys.stdin.read().split()))
 
 def postorder(start, end):
     if start > end:
         return
+
     root = preorder[start]
 
-    # 왼쪽/오른쪽 구간 나누기
     idx = start + 1
     while idx <= end and preorder[idx] < root:
         idx += 1
 
-    postorder(start + 1, idx - 1)  # 왼쪽
-    postorder(idx, end)            # 오른쪽
-    print(root)                     # 루트
+    # 왼쪽
+    postorder(start + 1, idx - 1)
+    # 오른쪽
+    postorder(idx, end)
+    # 루트
+    print(root)
 
 postorder(0, len(preorder) - 1)
+
+
