228610b25166aac8198fc1b77f8799ad23b2a2f7
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-07-26 12:26:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 128 ms, Memory: 114560 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,32 +1,32 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
+input = sys.stdin.readline
 
-n,m,v = map(int,input().split())
+n, m, v = map(int,input().split())
 
-visited = [False]*(n+1)
 graph = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
 
 for _ in range(m):
-    a,b = map(int, input().split())
+    a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
 
 for i in range(1,n+1):
     graph[i].sort()
 
-def dfs(start):
-    visited[start] = True
-    print(start, end=' ')
-
-    for nxt in graph[start]:
+def dfs(x):
+    visited[x] = True
+    print(x, end=' ')
+    for nxt in graph[x]:
         if not visited[nxt]:
             dfs(nxt)
 
-def bfs(start):
-    queue = deque([start])
+
+def bfs(x):
+    queue = deque([x])
     visited = [False]*(n+1)
-    visited[start] = True
+    visited[x] = True
     
     while queue:
         node = queue.popleft()
