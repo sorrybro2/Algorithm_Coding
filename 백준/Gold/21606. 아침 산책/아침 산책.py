54ffba1ea80b38dd5a85911fe7164534996797ff
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-07-28 23:58:54 +0900

    [Gold III] Title: 아침 산책, Time: 916 ms, Memory: 105788 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,39 +1,41 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 n = int(input())
-place = list(map(int, input().strip()))  # 정수 리스트
-graph = [[] for _ in range(n+1)]
+place = list(map(int, input().strip()))  # 0: 실외, 1: 실내
 
+graph = [[] for _ in range(n+1)]
 for _ in range(n-1):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
 visited = [False] * (n+1)
-cnt = 0
+answer = 0
 
+# --- DFS로 실외 컴포넌트 탐색 후 인접 실내 노드 카운트 ---
 def dfs(x):
     visited[x] = True
     indoor_count = 0
     for nxt in graph[x]:
-        if place[nxt-1] == 1:
+        if place[nxt-1] == 1:          # 실내
             indoor_count += 1
-        elif not visited[nxt]:
+        elif not visited[nxt]:         # 실외
             indoor_count += dfs(nxt)
     return indoor_count
 
-# 실외(0) 노드들만 DFS 탐색
+# 1) 실외 컴포넌트마다 계산
 for i in range(1, n+1):
     if place[i-1] == 0 and not visited[i]:
-        c = dfs(i)       # 해당 컴포넌트의 실내 노드 수
-        cnt += c * (c - 1)  # 조합 계산
+        c = dfs(i)
+        answer += c * (c-1)  # 조합
 
-# 인접한 실내 노드끼리 직접 연결된 경우도 더해줌
+# 2) 실내-실내 직접 연결 카운트
 for i in range(1, n+1):
     if place[i-1] == 1:
         for nxt in graph[i]:
             if place[nxt-1] == 1:
-                cnt += 1
+                answer += 1
 
-print(cnt)
+print(answer)
