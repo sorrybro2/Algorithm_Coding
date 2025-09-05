e1f3a5e8926399852bc1cece2a8b265bc9061a53
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-09-05 10:12:36 +0900

    [Bronze V] Title: A+B - 3, Time: 100 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -1,9 +1,8 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync(0, 'utf8').trim().split('\n');
 
-const count = parseInt(input[0]);
+const T = Number(input[0]);
 
-for(let i = 1; i <= count; i++){
-    const [a,b] = input[i].split(' ').map(Number);
+for(let i = 1; i <= T; i++){
+    const[a,b] = input[i].split(' ').map(Number);
     console.log(a+b);
 }
