cd5335b2729165b47d9da06b139c2f8a23fd82a5
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-09-07 20:55:20 +0900

    [Bronze III] Title: 최댓값, Time: 88 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,17 +1,13 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync(0, 'utf8').trim().split('\n');
 
-const a = input.map(Number);
+let list = [];
 
-let max = a[0];
-let n = 1;
-
-for(let i = 0; i < 9; i++){
-    if(max < a[i]){
-        max = a[i];
-        n = (i+1);
-    }
+for (let i = 0; i < 9; i++){
+    list[i] = Number(input[i]);
 }
 
-console.log(max);
-console.log(n);
+const MaxValue = Math.max(...list);
+const num = list.indexOf(MaxValue)+1;
+
+console.log(MaxValue);
+console.log(num);
