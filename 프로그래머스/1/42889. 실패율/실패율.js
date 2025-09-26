58f312330a6b44952203fccf0f90a404b8e7a1c2
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-09-26 20:07:03 +0900

    [level 1] Title: 실패율, Time: 6930.08 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,24 +1,19 @@
 function solution(N, stages) {
     var answer = [];
     
-    // 실패율을 구하기 위한 반복문
+    // 반복문
     for(let i = 1; i <= N; i++){
+        const fail = stages.filter(item => item==i).length;
+        const stagelng = stages.length;
         
-        // 해당 스테이지에서의 실패 개수와 전체 개수를 파악 후 실패율 구함
-        const failcnt = stages.filter(stage => stage == i).length;
-        const stage = stages.length;
-        const failRate = failcnt/stage;
+        const failRate = fail/stagelng;
         
-        // 키와 값으로 넣음!
         answer.push({stage : i, rate : failRate});
         
-        // i 스테이지는 빼줘야함
-        stages = stages.filter(x => x != i);
+        stages = stages.filter(item => item != i);
     }
     
-    // 실패율 기준 내림차순
-    answer.sort((a,b) => b.rate - a.rate);
+    answer.sort((a,b) => b.rate-a.rate);
     
-    // 답이 나와야하는데 stage만 나와야함
-    return answer.map(item => item.stage);
+    return answer.map(i => i.stage);
 }
