f28a5604aa854bd8d7a7e7c2d6644d61f2d614d0
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-09-27 17:16:02 +0900

    [level 2] Title: 방문 길이, Time: 1.22 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,49 +1,39 @@
-// 방향키 세팅
-function updateLocation(x, y, dir){
-    switch(dir){
-        case "U":
+function direction(x, y, dir){
+    switch (dir){
+        case "U" : 
             return [x, y+1];
-        case "D":
+        case "D" :
             return [x, y-1];
-        case "L":
-            return [x-1, y];
-        case "R":
+        case "R" :
             return [x+1, y];
+        case "L" :
+            return [x-1, y];
     }
 }
 
-// 범위 세팅
-function canMove(nx,ny){
-    return nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5
+function square5x5(nx, ny){
+    return (nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5);
 }
-
-
 
 function solution(dirs) {
-    // 초기 좌표 (0,0)
     let x = 0;
     let y = 0;
-    
-    // 중복은 없애기로~
-    const visited = new Set();
-    
-    // 반복문으로 방향키 받음
+
+    let visited = new Set();
+
     for(const dir of dirs){
-        // 좌표 업데이트!
-        const [nx, ny] = updateLocation(x,y,dir);
-        // 죄표 범위 아니면
-        if(!canMove(nx, ny)){
-            // continue로 받지 않음
+        const [nx, ny] = direction(x,y,dir);
+        if (!square5x5(nx,ny)){
             continue;
         }
+
         
-        // 양향방으로 받음
-        visited.add(`${x}${y}${nx}${ny}`);
         visited.add(`${nx}${ny}${x}${y}`);
-        
-        // 좌표 이동!
-        [x, y] = [nx, ny];
+        visited.add(`${x}${y}${nx}${ny}`);
+
+        [x,y] = [nx, ny];
     }
-    
-    return visited.size/2;
+
+    var answer = visited.size/2;
+    return answer;
 }
