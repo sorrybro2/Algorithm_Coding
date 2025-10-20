9a00075732bdc7469d6ee307a19535e84a963406
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-10-20 16:44:05 +0900

    [level 2] Title: 전화번호 목록, Time: 156.66 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,15 +1,17 @@
 function solution(phone_book) {
     
+    // 사전 순으로 오름차순임!
+    phone_book.sort();
+    
+    // 전화번호부에 접두사 관계인 연락처가 없다면 true
     var answer = true;
     
-    phone_book.sort();
+    // 접두사 관계가 있을 시, false
     for(let i = 1; i < phone_book.length; i++){
-            if (phone_book[i].startsWith(phone_book[i-1])){
-                answer = false;
-                return answer;
-            
+        if(phone_book[i].startsWith(phone_book[i-1])){
+            answer = false;
         }
     }
-    
+        
     return answer;
 }
