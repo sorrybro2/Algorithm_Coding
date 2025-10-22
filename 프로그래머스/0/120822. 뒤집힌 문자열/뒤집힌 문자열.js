22c01a0a84fe1bfd96fdbd6f3005e414560b9fc1
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-10-22 18:41:56 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
@@ -1,9 +1,8 @@
 function solution(my_string) {
-    var answer = '';
     
-    for(let i = my_string.length-1; i >= 0; i--){
-        answer += my_string[i];
-    }
+    // 스프레드 연산자를 사용하면 ["j","a","r","o","n"]으로 분리되므로
+    // reverse() 함수를 통해 거꾸로 뒤집은 문자열을 만들고 join('')으로 합침!
+    var answer = [...my_string].reverse().join('');
     
     return answer;
 }
