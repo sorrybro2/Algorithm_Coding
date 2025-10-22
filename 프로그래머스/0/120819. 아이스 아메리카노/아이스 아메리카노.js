388af3469e9da825deb6354c45371737cbcddbed
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-10-22 20:40:08 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,8 +1,3 @@
 function solution(money) {
-    var answer = [];
-    const coffee_cnt = Math.floor(money/5500);
-    const change = money - 5500 * coffee_cnt;
-    answer.push(coffee_cnt);
-    answer.push(change);
-    return answer;
+    return [Math.floor(money/5500), money % 5500];
 }
