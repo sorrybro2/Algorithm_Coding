b7e2a4a0bf5972188ab1826151883e53a7af3b1e
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-10-22 19:38:39 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -1,7 +1,4 @@
 function solution(numbers, num1, num2) {
-    var answer = [];
-    for(let i = num1; i <= num2; i++){
-        answer.push(numbers[i]);
-    }
+    var answer = numbers.splice(num1, num2-num1+1);
     return answer;
 }
