c383aa3344ffd0808737afdba74f7b3ef80fa0a8
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-10-24 20:14:42 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,14 +1,3 @@
 function solution(hp) {
-    let answer = 0;
-    answer += Math.trunc(hp/5);
-    hp %= 5;
-    if(hp){
-        answer += Math.trunc(hp/3);
-        hp %= 3;
-    }
-    if(hp){
-        answer += hp;
-    }
-    
-    return answer;
+    return Math.trunc(hp/5) + Math.trunc((hp%5)/3) + hp%5%3;
 }
