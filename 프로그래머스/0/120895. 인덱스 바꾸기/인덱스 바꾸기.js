c0a5abda1b8be23964722abbb5fb3858037e8505
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-10-26 18:57:23 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,10 @@
+function solution(my_string, num1, num2) {
+    
+    let arr = my_string.split('');
+    
+    [arr[num1], arr[num2]] = [arr[num2] , arr[num1]];
+    
+    var answer = arr.join('');
+    
+    return answer;
+}
