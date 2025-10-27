a784cf31cbd1edb9cb4b4917d17fbc83c775a2ca
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-10-27 16:54:04 +0900

    [level 0] Title: 합성수 찾기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,11 +1,10 @@
 function solution(n) {
     answer = 0;
     for(let i = 4; i <= n; i++){
-        let tri_bool = true;
         for(let j = 2; j < i; j++){
-            if(i%j==0 && tri_bool){
+            if(i%j==0){
                 answer += 1;
-                tri_bool = false;
+                break;
             }
         }
     }
