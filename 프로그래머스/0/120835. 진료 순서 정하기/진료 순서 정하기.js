6341d300fa0052279545abd318e202280b649040
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-10-30 21:14:22 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,10 +1,10 @@
 function solution(emergency) {
+    // 원래 배열과 내림차순을 해놓은 배열을 만들고
+    // 원래 배열 기준으로 내림차순 배열의 인덱스 + 1 값을 차례로 출력하면 끝
     
     const original = [...emergency];
-
+    
     const sorted = [...emergency].sort((a,b) => b-a);
     
-    const answer = original.map(v => sorted.indexOf(v)+1);
-    
-    return answer;
+    return original.map(v => sorted.indexOf(v)+1);
 }
