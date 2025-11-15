9fe54c4e14ea10efb80dcd6a7ad8450a71d8cc7b
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-11-15 17:23:14 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,8 +1,10 @@
-ary = []
+list = []
 
-for i in range(9):
-    x = int(input())
-    ary += [x]
-
-print(max(ary))
-print(ary.index(max(ary))+1)
+for i in range(1,10):
+    n = int(input())
+    list.append(n)
+    
+max_num = max(list)
+print(max_num)
+max_idx = list.index(max_num)+1
+print(max_idx)
