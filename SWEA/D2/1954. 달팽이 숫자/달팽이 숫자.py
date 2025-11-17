b4f868ddcfa636e3ad9b42cc7016355291113eb1
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-11-17 17:48:29 +0900

    [D2] Title: 달팽이 숫자, Time: 63 ms, Memory: 50,944 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,29 +1,28 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    N = int(input())
-    board = [[0]*N for _ in range(N)]
+for tc in range(1,T+1):
+	dx = [0, 1, 0, -1]
+	dy = [1, 0, -1, 0]
+    
+	idx = 0
+	num = 1
+    
+	x, y = 0, 0
+    
+	N = int(input())
+	board = [[0]*N for _ in range(N)]
+    
+	for i in range(N*N):
+		board[x][y] = num
+		nx, ny = x+dx[idx], y+dy[idx]
+		if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0:
+			x, y = nx, ny
+		else:
+			idx = (idx+1)%4
+			x, y = x+dx[idx], y+dy[idx]
+		num += 1
 
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
-    idx = 0
-
-    x, y = 0, 0
-    num = 1
-
-    for _ in range(N*N):
-        board[x][y] = num
-        nx = x + dx[idx]
-        ny = y + dy[idx]
-
-        if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0:
-            x, y = nx, ny
-        else:
-            idx = (idx + 1) % 4
-            x, y = x + dx[idx], y + dy[idx]
-
-        num += 1
-
-    print(f"#{test_case}")
-    for row in board:
-        print(' '.join(map(str, row)))
+	print(f"#{tc}")
+	for row in board:
+		print(' '.join(map(str, row)))
+            
