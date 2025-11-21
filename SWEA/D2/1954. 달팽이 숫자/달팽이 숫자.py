47d8a3d558ad90bc89874ce58c27803146cb6436
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-11-21 15:53:45 +0900

    [D2] Title: 달팽이 숫자, Time: 65 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,28 +1,23 @@
 T = int(input())
-
-for tc in range(1,T+1):
+for tc in range(1, T+1):
+	n = int(input())
+	
 	dx = [0, 1, 0, -1]
 	dy = [1, 0, -1, 0]
-    
 	idx = 0
-	num = 1
-    
 	x, y = 0, 0
-    
-	N = int(input())
-	board = [[0]*N for _ in range(N)]
-    
-	for i in range(N*N):
-		board[x][y] = num
-		nx, ny = x+dx[idx], y+dy[idx]
-		if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0:
+
+	board = [[0]*n for _ in range(n)]
+	
+	for i in range(1, n*n+1):
+		board[x][y] = i
+		nx, ny = x + dx[idx], y + dy[idx]
+		if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
 			x, y = nx, ny
 		else:
-			idx = (idx+1)%4
-			x, y = x+dx[idx], y+dy[idx]
-		num += 1
-
+			idx = (idx+1) % 4
+			x, y = x + dx[idx], y + dy[idx]
+	
 	print(f"#{tc}")
 	for row in board:
 		print(' '.join(map(str, row)))
-            
