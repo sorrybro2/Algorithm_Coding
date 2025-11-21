8da0b7a453d42ce3e1de399704997ef5947bc181
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-11-21 16:15:06 +0900

    [D2] Title: 파리 퇴치, Time: 74 ms, Memory: 58,624 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -2,13 +2,15 @@ T = int(input())
 for tc in range(1, T+1):
 	N, M = map(int, input().split())
 	board = [list(map(int, input().split())) for _ in range(N)]
-	max_catch = 0
-	for i in range(N-M+1):
-		for j in range(N-M+1):
-			fly = 0
-			for k in range(i, i+M):
-				for l in range(j, j+M):
-					fly += board[k][l]
-			if max_catch < fly:
-				max_catch = fly
+	max_catch = 0	
+
+	for i in range (N-M+1):
+		for j in range (N-M+1):
+			total = 0
+			for k in range (i, i+M):
+				for l in range (j, j+M):
+					total += board[k][l]
+			if max_catch < total:
+				max_catch = total
+
 	print(f"#{tc} {max_catch}")
