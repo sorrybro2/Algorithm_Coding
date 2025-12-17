e3ba0444a133bc26e85c5772cb8efc568557d29a
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-12-17 19:20:56 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.py
+++ 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.py
@@ -1,11 +1,2 @@
-t1 = int(input())
-t2 = int(input())
-t3 = int(input())
-t4 = int(input())
-
-total = t1+t2+t3+t4
-
-if total <= 1500:
-    print("Yes")
-else:
-    print("No")
+total = sum([int(input()) for _ in range(4)])
+print("Yes" if total <= 1500 else "No")
