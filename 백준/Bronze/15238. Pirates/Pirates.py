73b97a7dab7ef936eba8e156fb42e8a06b490db0
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-12-23 13:06:42 +0900

    [Bronze III] Title: Pirates, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15238. Pirates/Pirates.py
+++ 백준/Bronze/15238. Pirates/Pirates.py
@@ -1,13 +1,13 @@
 n = int(input())
 word = input()
 
-alpha = [0] * 26
+alphabet = [0] * 26
 
-for l in word:
-    idx = ord(l) - ord('a')
-    alpha[idx] += 1
+for c in word:
+    idx = ord(c) - ord('a')
+    alphabet[idx] += 1
 
-max_value = max(alpha)
-max_alpha = chr(alpha.index(max_value)+97)
+max_value = max(alphabet)
+max_alpha = chr(alphabet.index(max_value)+97)
 
 print(f"{max_alpha} {max_value}")
